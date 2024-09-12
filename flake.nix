c02840579cae55fd8b6bb4b533b81d5c5c5f57a3
Wilfried Chauveau <wilfried.chauveau@arm.com>
2024-09-12 06:52:24 +0100

    Remove unused python related code

--- flake.nix
+++ flake.nix
@@ -3,18 +3,10 @@
 {
   description = "The real nix file";
   inputs = {
-    dream2nix = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/dream2nix";
-    };
     flake-utils.url = "github:numtide/flake-utils";
     flakegen.url = "github:jorsn/flakegen";
     nixpkgs.url = "nixpkgs/nixos-24.05";
     nixpkgs_python38.url = "nixpkgs/nixos-23.11";
-    pyocd = {
-      flake = false;
-      url = "github:pyocd/pyocd";
-    };
     sdk_linux-aarch64 = {
       flake = false;
       url = "file+https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.8/zephyr-sdk-0.16.8_linux-aarch64_minimal.tar.xz";
@@ -62,10 +54,6 @@
     toolchain_macos-x86_64_arm-zephyr-eabi = {
       flake = false;
       url = "file+https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.8/toolchain_macos-x86_64_arm-zephyr-eabi.tar.xz";
-    };
-    zephyr = {
-      flake = false;
-      url = "github:zephyrproject-rtos/zephyr";
     };
   };
   outputs = inputs: inputs.flakegen ./flake.in.nix inputs;
